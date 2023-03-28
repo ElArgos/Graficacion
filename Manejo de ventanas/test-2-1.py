4f45257f49c2fc5418ddcb51d8fa1286ae2199c3
ElArgos <123092549+ElArgos@users.noreply.github.com>
2023-03-28 10:10:59 -0600

    Add files via upload

---
+++ Manejo de ventanas/test-2-1.py
@@ -0,0 +1,8 @@
+from Base import Base 
+class Test(Base):
+ def initialize(self):
+  print("Initializing program...")
+ def update(self):
+  pass 
+# instantiate this class and run the program 
+Test().run()
